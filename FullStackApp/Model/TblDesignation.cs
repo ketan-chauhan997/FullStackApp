aaa8de850101118bbd930a949dd980c5fda2550a
ketan-chauhan997 <chauhan.25997@gmail.com>
2023-10-04 16:53:52 +0530

    Package Libraires and Models Creation

---
+++ FullStackApp/Model/TblDesignation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackApp.Model
+{
+    public class TblDesignation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DesignationId { get; set; }
+        public string Designation { get; set; }
+    }
+}
---
+++ FullStackApp/Model/TblEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackApp.Model
+{
+    public class TblEmployee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+        [StringLength(150)]
+        public string Name { get; set; }
+        [StringLength(150)]
+        public string LastName { get; set; }
+        [StringLength(250)]
+        public string EmailId { get; set; }
+        public int Age { get; set; }
+        public DateTime DOJ { get; set; }
+        [StringLength(20)]
+        public string Gender { get; set; }
+        public int IsMarried { get; set; }
+        public int IsActive { get; set; }
+        public int DesignationId { get; set; }
+        [NotMapped]
+        public string Designation { get; set; }
+    }
+}
